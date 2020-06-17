eb870a8f5022ec5afa209f1f7033c88b27fae84b
yamaguchiai <yamaguchiai@192.168.0.29>
2020-06-17 15:27:14 +0900

    最初にテストのためのコミットをします

---
+++ IntToEng/src/IntToEng.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class IntToEng {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		  Scanner sc = new Scanner(System.in);
+	        int input = sc.nextInt();
+	        System.out.println(translateEng(input));
+	    }
+
+	    // 数値を英訳する変換するメソッド
+	    static String translateEng(int n) {
+	        return "";
+
+	}
+
+}
